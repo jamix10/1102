537174a479949910dcc7aa6fd156a906bd6023e4
Andrew <jamix10@gmail.com>
2017-02-17 19:06:39 +0200

    Update age_request1.py

--- 13/age_request1.py
+++ 13/age_request1.py
@@ -2,6 +2,6 @@ cr = 2017
 name = str(input('Введите имя > '))
 year = int(input('Введите год рождения >'))
 age = cr - year
-msg = "{0} {1}".format(str(name), str(age))
+msg = '{0} {1}'.format(str(name), str(age))
 
 print(msg)
