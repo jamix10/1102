19c0c4c569852d7f0882d28a7cb48a268512abd0
Andrew <jamix10@gmail.com>
2017-02-17 23:16:40 +0200

    Update hw2_task5.py

--- 13/hw2_task5.py
+++ 13/hw2_task5.py
@@ -1,4 +1,4 @@
-# Объявление всех переменных
+# Объявление переменных
 std_name = str(input('Введите имя >'))
 num_lab = str(input('Введите номер работы >'))
 num_gr = str(input('Введите номер группы >'))
@@ -8,13 +8,13 @@ gr = str('Выполнил студент гр. № ')
 line1 = lab + num_lab
 line2 = gr + num_gr
 line3 = std_name
-
+# определение максимальной ширины рамки
 width_frame = max(len(line1),len(line2),len(line3))
-
+# оперделение множиеля для пробелов перед краем рамки
 ls1 = (width_frame ) - (len(line1))
 ls2 = (width_frame ) - (len(line2))
 ls3 = (width_frame ) - (len(line3))
-
+# вывод рамки
 print('*' * (width_frame + 4))
 print('*',line1,' ' * (ls1) ,'*')
 print('*',line2,' ' * (ls2) ,'*')
