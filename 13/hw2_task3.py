bb5f8784352b96f3f9ba29aa9f95540316fe0ab5
Andrew <jamix10@gmail.com>
2017-02-17 19:05:19 +0200

    Add files via upload

--- 13/hw2_task3.py
+++ 13/hw2_task3.py
@@ -1,7 +1,6 @@
 for a in [[1, 2, 3], [4, 5, 6], [7, 8, 9]]:
-    if a != 0:
         print('-' * 20)
-        for b in range(2, 10):
-            print('{}x{}={}'.format(a[0], b, a[0] * b),
-                  '{}x{}={}'.format(a[1], b, a[0] * b),
-                  '{}x{}={}'.format(a[2], b, a[0] * b))
+        for b in range(1, 10):
+            print('{}x{}={}\t'.format(a[0], b, a[0] * b),
+                  '{}x{}={}\t'.format(a[1], b, a[0] * b),
+                  '{}x{}={}\t'.format(a[2], b, a[0] * b))
