21546fa81aea068a68cf805ef9bb75c37571d6f6
Andrew <jamix10@gmail.com>
2017-02-18 19:04:44 +0200

    Update hw3_task1.py

--- 13/hw3_task1.py
+++ 13/hw3_task1.py
@@ -1,7 +1,13 @@
-num = int(input('Введите число > '))
-def chek (num):
-    if num % 2 == 0:
-        print(True)
+num = int (input('Ведите число > '))
+def is_prime(num):
+    i = 2
+    j = 0
+    while i ** 2 <= num and j != 1:
+        if num % i == 0:
+            j = 1
+        i += 1
+    if j == 1:
+        print("Это составное число")
     else:
-        print(False)
-chek(num)
+        print("Это простое число")
+is_prime(num)
